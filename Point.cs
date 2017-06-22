e5f710baa3ef05875c18e86b90c7b3994aa662ae
Oneclick2016 <fallenange.trololo@gmail.com>
2017-06-22 09:04:44 +0400

    Созданы классы для вывода в консоль текста

---
+++ Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karina
+{
+    class Point
+    {
+        public Point(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(80, 25);
+
+            //-------------------------I
+            Point p = new Point(40, 2, '+');
+            Point p1 = new Point(41, 2, '+');
+            Point p2 = new Point(39, 2, '+');
+
+            Point p3 = new Point(40, 3, '+');
+            Point p4 = new Point(40, 4, '+');
+            Point p6 = new Point(40, 5, '+');
+            Point p7 = new Point(40, 6, '+');
+
+            Point p8 = new Point(40, 7, '+');
+            Point p9 = new Point(41, 7, '+');
+            Point p10 = new Point(39, 7, '+');
+
+            //--------------------------L
+            Point p11 = new Point(27, 9, '+');
+            Point p12 = new Point(27, 10, '+');
+            Point p13 = new Point(27, 11, '+');
+            Point p14 = new Point(27, 12, '+');
+            Point p15 = new Point(27, 13, '+');
+            Point p16 = new Point(28, 13, '+');
+            Point p17 = new Point(29, 13, '+');
+            Point p18 = new Point(30, 13, '+');
+
+            //----------------------------O
+            Point p11 = new Point(27, 9, '+');
+            Point p12 = new Point(27, 10, '+');
+            Point p13 = new Point(27, 11, '+');
+            Point p14 = new Point(27, 12, '+');
+            Point p15 = new Point(27, 13, '+');
+            Point p16 = new Point(28, 13, '+');
+            Point p17 = new Point(29, 13, '+');
+            Point p18 = new Point(30, 13, '+');
+
+            Console.ReadLine();
+        }
+    }
+}
